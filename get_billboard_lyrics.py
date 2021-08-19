b0c23b946e6c4849d8e0ed3a693a1b1ffbbd352f
FelixSiegfriedRiedel <f.riedel.business@gmail.com>
2021-08-20 00:38:29 +0200

    changed algorithm to search only by first artist name

--- get_billboard_lyrics.py
+++ get_billboard_lyrics.py
@@ -7,50 +7,21 @@ token_file = open("genius_token.txt", "r")
 token = token_file.read()
 token_file.close()
 
-genius = lyricsgenius.Genius(token, response_format='plain', timeout=5, sleep_time=0.1)
+genius = lyricsgenius.Genius(token, response_format='plain', timeout=3, sleep_time=0.1)
 genius.verbose = False
 genius.remove_section_headers = True
 genius.skip_non_songs = True
-genius.retries = 2
-
-
-def get_artist(artist):
-    try:
-        genius_artist = genius.search_artist(artist, max_songs=1)
-        name = genius_artist.name
-    except:
-        name = '!Error!'
-
-    return name
-
+genius.retries = 1
 
 def get_lyrics(title, artist):
     try:
-        genius_artist = get_artist(artist)
-        if genius_artist != '!Error!':
-            song = genius.search_song(title, genius_artist)
-        else:
-            song = genius.search_song(title, artist)
-
+        song = genius.search_song(title, artist)
         if song:
             lyrics = song.lyrics.replace('\n', ' ').replace('\r', '')
             print('#', end='')
         else:
             lyrics = '!NoSong!'
             print('X', end='')
-
-
-
-    # except HTTPError:
-    #     lyrics = '!HTTPError!'
-    #     print('E', end='')
-    # except Timeout:
-    #     lyrics = '!Timeout!'
-    #     print('E', end='')
-    # except TypeError:
-    #     lyrics = '!TypeError!'
-    #     print('E', end='')
-
     except:
         lyrics = '!Error!'
         print('E', end='')
@@ -62,19 +33,19 @@ artist_song = pd.read_json('data/artist_song/artist_song.json')
 artist_song['lyrics'] = ''
 
 # updates csv file every 20 entries
-for i in range(0, 1281, 20):
+for i in range(0, 1301, 20):
     j = i + 20
     safe = artist_song.iloc[i:j].copy()
-    safe['lyrics'] = np.vectorize(get_lyrics)(safe['song'], safe['artist'])
+    safe['lyrics'] = np.vectorize(get_lyrics)(safe['song'], safe['first_artist'])
     artist_song['lyrics'].update(safe['lyrics'])
     artist_song.to_csv('data/lyrics/artist_song_lyrics.csv')
     # indicator showing progress
-    print('\n', j, '/', 1309)
+    print('\n', j, '/', 1322)
 
 # adds the remaining lyrics
-safe = artist_song.iloc[1301:].copy()
-safe['lyrics'] = np.vectorize(get_lyrics)(safe['song'], safe['artist'])
+safe = artist_song.iloc[1321:].copy()
+safe['lyrics'] = np.vectorize(get_lyrics)(safe['song'], safe['first_artist'])
 artist_song['lyrics'].update(safe['lyrics'])
 artist_song.to_csv('data/lyrics/artist_song_lyrics.csv', encoding='utf-8')
-print('\n', 1309, '/', 1309)
+print('\n', 1322, '/', 1322)
 print(' Done!')
