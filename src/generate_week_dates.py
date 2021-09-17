b6926437fd64fdf4d3803fc271a33de23cf22cab
FelixSiegfriedRiedel <f.riedel.business@gmail.com>
2021-09-17 13:51:30 +0200

    changed project sturcture

--- src/generate_week_dates.py
+++ src/generate_week_dates.py
@@ -1,19 +1,15 @@
 import datetime
 
 
-def generate_week_dates(start_year, *args):
+def generate_week_dates(start_year, end_year):
     arr = []
-    upper_week_range = 53
-    if args[0]:
-        end_year = args[0]+1
-    else:
-        end_year = start_year+1
-    if args[1]:
-        upper_week_range = args[1]+1
     for year in range(start_year, end_year):
-        for week in range(1, upper_week_range):
+        for week in range(0, 52):
             week_string = str(year)+'-W'+str(week)
             week_date = datetime.datetime.strptime(week_string + '-6', "%Y-W%W-%w")
             week_date_string = str(week_date).split(' ')[0]
             arr.append(week_date_string)
     return arr
+
+
+print(generate_week_dates(2019, 2020))
