a169752e5f8e2729b37458ac8928c16ad15f0c44
FelixSiegfriedRiedel <f.riedel.business@gmail.com>
2021-10-12 13:38:08 +0200

    updated crawler

--- src/get_billboard_lyrics.py
+++ src/get_billboard_lyrics.py
@@ -16,36 +16,79 @@ genius.remove_section_headers = True
 genius.skip_non_songs = True
 genius.retries = 1
 
-def get_lyrics(title, artist):
+def get_genius_resources(title, artist):
+    result_dict = {}
     try:
         song = genius.search_song(title, artist)
         if song:
-            lyrics = song.lyrics
-            url = 'https://genius.com' + song.path
+            result_dict['lyrics'] = song.lyrics
+            result_dict['url'] = 'https://genius.com' + song.path
+            id = song.id
+            result_dict['id'] = id
+            result_dict['primary_artist'] = song.primary_artist
+            song = genius.song(song.id)['song']
+            result_dict['description'] = song['description']['plain']
+            result_dict['annotations'] = genius.song_annotations(id)
+            result_dict['comments'] = []
+            for comment in genius.song_comments(id, per_page=20)['comments']:
+                result_dict['comments'].append(comment['body']['plain'])
+            result_dict['release_date'] = song['release_date']
         else:
-            lyrics = '!NoSong!'
-            url = ''
+            result_dict['lyrics'] = '!NoSong!'
+            result_dict['url'] = ''
+            result_dict['id'] = ''
+            result_dict['primary_artist'] = ''
+            result_dict['description'] = ''
+            result_dict['annotations'] = []
+            result_dict['comments'] = []
+            result_dict['release_date'] = ''
     except:
-        lyrics = '!Error!'
-        url = ''
+        result_dict['lyrics'] = '!Error!'
+        result_dict['url'] = ''
+        result_dict['id'] = ''
+        result_dict['primary_artist'] = ''
+        result_dict['description'] = ''
+        result_dict['annotations'] = []
+        result_dict['comments'] = []
+        result_dict['release_date'] = ''
+    return result_dict
 
-    return lyrics, url
+
 
 
 lyrics_list = []
 url_list = []
+id_list = []
+primary_artist_list = []
+description_list = []
+annotations_list = []
+comments_list = []
+release_date_list = []
 
 artist_song = pd.read_json('../data/artist_song/artist_song.json')
 length = len(artist_song)
 printProgressBar(0, length, prefix='Progress:', suffix='Complete', length=50)
 
 for i in range(0, length):
-    lyrics, url = get_lyrics(artist_song['song'].iloc[i], artist_song['artist'].iloc[i])
-    lyrics_list.append(lyrics)
-    url_list.append(url)
+    resources = get_genius_resources(artist_song['song'].iloc[i], artist_song['artist'].iloc[i])
+    lyrics_list.append(resources['lyrics'])
+    url_list.append(resources['url'])
+    id_list.append(resources['id'])
+    primary_artist_list.append(resources['primary_artist'])
+    description_list.append(resources['description'])
+    annotations_list.append(resources['annotations'])
+    comments_list.append(resources['comments'])
+    release_date_list.append((resources['release_date']))
     printProgressBar(i + 1, length, prefix='Progress:', suffix='Complete', length=50)
 
 artist_song['lyrics'] = lyrics_list
 artist_song['url'] = url_list
+artist_song['genius_id'] = id_list
+artist_song['genius_primary_artist'] = primary_artist_list
+artist_song['genius_description'] = description_list
+artist_song['genius_annotations'] = annotations_list
+artist_song['genius_comments'] = comments_list
+artist_song['release_date'] = release_date_list
 
-artist_song.to_csv('data/lyrics/bb-t100-lyrics.csv', encoding='utf-8')
+artist_song.to_csv('../data/lyrics/bb-t100-lyrics_new.csv', encoding='utf-8')
+artist_song.to_excel('../data/output/bb-t100-lyrics_new.xlsx')
